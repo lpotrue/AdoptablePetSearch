3f8ea17ab91512e3b073eadf7494de537f7851b5
Lindsay Potrue <lindsaypotrue@comcast.net>
2017-07-20 13:01:23 -0700

    added photos

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
  $(document).ready(function(){
-    var url = 'http://api.petfinder.com/pet.getRandom?key=a4d4d400939b10647da19b7593286b34&shelterid=KY305&output=full&format=json';
+   
+    $('form').on('submit', function(e){
+        e.preventDefault(); 
+        console.log($('input').val())
+        makeApiCall($('input').val())
+    })
+
+
+});
+    makeApiCall(97219)
+//http://api.petfinder.com/pet.getRandom?format=json&key=a4d4d400939b10647da19b7593286b34&animal=dog&output=basic
+function makeApiCall(zipcode){
+
+ //var zipcode = (zipcode);
+    var url = `http://api.petfinder.com/pet.find?location=${zipcode}&key=a4d4d400939b10647da19b7593286b34&output=full&format=json`;
     $.ajax({
         type : 'GET',
         data : {},
         url : url+'&callback=?' ,
         dataType: 'json',
         success : function(data) {              
-            console.log(data)
-            var result = '';
+           // console.log(data)
+            //for(var i =0; i<20; i++){
+               // console.log(data.petfinder.pets.pet[i].description.$t)
+           // }
+           let html = ""
+           let pets = data.petfinder.pets.pet;
+           pets.forEach(function(pet,e) { 
+            
 
-            var petfinder = data.petfinder;
-            var infoHTML = '<ul>';
-            infoHTML += '<li>';
-            infoHTML += '<strong>Description</strong><br>';
-            infoHTML += petfinder.pet.description['$t'];
-            infoHTML += '</li>';
+                console.log(pet,e) 
+                let name = pet.name.$t;
+                html += '<h4>'+name+'</h4>'
+                let description = pet.description.$t;
+                //html += '<h5>'+description+ '</h5>'
+                //html +='<img src ="http://photos.petfinder.com/photos/pets/38751785/1/?bust=1499974270&width=50&-t.jpg"/>'
+                //html += '<img src ="http://photos.petfinder.com/photos">'
+                for(var k = 0; k<pet.media.photos.photo.length; k++){
+                    if(pet.media.photos.photo[k]['@size'] == "x") {
+                        html +=`<img src = "${pet.media.photos.photo[k].$t}"/>`
+                    }
 
-            infoHTML += '</li>';
+                }
+                html += `<h5>${description}</h5>` 
+                //objective is to show all images for each pet. This will require an additional for each loop within the current for each loop.
+            } )
+           
+        
+            
 
-            infoHTML += '</ul>';
-            // return infoHTML;
-            $('#petfinderInfo').html(infoHTML);
+           $('#petfinderInfo').html(html)
 
-            // $('#petfinderInfo').html(petfinder.pet.description['$t']);
-            // 
-            console.log(petfinder);
         },
         error : function(request,error)
         {
             alert("Request: "+JSON.stringify(request));
         }
     });
-});
-//http://api.petfinder.com/pet.getRandom?format=json&key=a4d4d400939b10647da19b7593286b34&animal=dog&output=basic
+}
