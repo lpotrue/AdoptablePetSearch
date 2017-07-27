6923660baaa2a333e3c3012da6d9a89575a4d581
Lindsay Potrue <lindsaypotrue@comcast.net>
2017-07-27 11:18:32 -0700

    Interim progress

--- map.js
+++ map.js
@@ -36,16 +36,3 @@ google.maps.event.addListener(map, 'click', function(event) {
 });
 
 }
-
-/*function initMap() {
-        var myLatLng = {lng: -104.991531, lat: 39.742043};
-        
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: myLatLng
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }*/
