6766cd40de5f5dfff24b5e05263ae6f302305154
Lindsay Potrue <lindsaypotrue@comcast.net>
2017-07-31 14:53:19 -0700

    Updated look and feel.

--- map.js
+++ map.js
@@ -1,12 +1,14 @@
 
 function initMap() {
-  var myLatlng = new google.maps.LatLng(41.850033, -87.6500523);
+  var myLatlng = new google.maps.LatLng(40.2010 , -98.9836);
   var myOptions = {
     zoom: 4, 
     scrollwheel: false,
     center: myLatlng
+
 }
 var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
 var geocoder = new google.maps.Geocoder()
 
 google.maps.event.addListener(map, 'click', function(event) {
@@ -15,21 +17,26 @@ google.maps.event.addListener(map, 'click', function(event) {
   }, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
       if (results[0]) {
-        //alert(results[0].formatted_address);
-        //console.log(results)
-        //console.log(results[0].address_components[7].long_name)
-
+        
+        
           results[0].address_components.forEach((result, i)=>{ 
             if (result.types[0]== "postal_code") {
               
               let zipcode = Number(result.long_name)
               console.log(zipcode)
-              makeApiCall(zipcode)
+             let zips = makeApiCall(zipcode)
+              console.log(zips)
+              var lat = results[0].geometry.location.lat();
+              var lng = results[0].geometry.location.lng();
+              var newLatLng = new google.maps.LatLng(lat , lng);
+              var marker = new google.maps.Marker({
+                position: newLatLng,
+                title:"Hello World!"
+              });
+              // To add the marker to the map, call setMap();
+              marker.setMap(map);   
             }
           })
-
-        
-
       }
     }
   });
--- script.js
+++ script.js
@@ -4,12 +4,14 @@
         e.preventDefault(); 
         console.log("The zip code is: " + $('input').val())
         makeApiCall($('input').val())
-        //var makeApiCall = parseInt($(this).val());
     })
+    });
 
 
-});
-   // makeApiCall(97219)
+
+//var zips = []
+    //makeApiCall(93427)
+
 //http://api.petfinder.com/pet.getRandom?format=json&key=a4d4d400939b10647da19b7593286b34&animal=dog&output=basic
 function makeApiCall(zipcode){
 //http://api.petfinder.com/shelter.get?id=SD08&format=json&key=a4d4d400939b10647da19b7593286b34&animal=smallfurry&output=basic
@@ -25,33 +27,70 @@ function makeApiCall(zipcode){
             //for(var i =0; i<20; i++){
                // console.log(data.petfinder.pets.pet[i].description.$t)
            // }
+           //zips = []
            let html = ""
            let pets = data.petfinder.pets.pet;
            pets.forEach(function(pet,e) { 
             
 
                 console.log(pet,e) 
-                console.log(pet.shelterId.$t)
+                console.log(pet.shelterId)
+                
                 let name = pet.name.$t;
-                html += '<h4>'+name+'</h4>'
-
-                //let breed = pet.breed.$t;
-                //html += `<h5>${breed}</h5>`
-                let description = pet.description.$t;
-                html += `<h5>${description}</h5>` 
-                //html += '<h5>'+description+ '</h5>'
-                //html +='<img src ="http://photos.petfinder.com/photos/pets/38751785/1/?bust=1499974270&width=50&-t.jpg"/>'
-                //html += '<img src ="http://photos.petfinder.com/photos">'
+                html += '<div class="name">'+name+'</div>'
                 for(var k = 0; k<pet.media.photos.photo.length; k++){
                     if(pet.media.photos.photo[k]['@size'] == "x") {
                         html +=`<img src = "${pet.media.photos.photo[k].$t}"/>`
+                        break
                     }
-
                 }
-                html += `<h5>${description}</h5>` 
+                let zip = pet.contact.zip.$t;
+                zips.push(zip)
+                //let coverPhoto = pet.media.photos.photo[0].$t;
+                //html +=`<img src = "${pet.media.photos.photo[0].$t}"/>`
+                let breed = pet.breeds.breed.$t;
+                if (pet.mix.$t == "yes") {
+                html += `<div id="breed">Amazing Mixed Breed</div>`
+                }
+                html += `<div id="breed">${breed}</div>`
+                let age = pet.age.$t;
+                html += `<div id="age">${age}</div>`
+                let size = pet.size.$t;
+                if (pet.size.$t == "s") {
+                html += `<div id="small">"I am " + $('input')</div>`
+                }
+                if(pet.size.$t == "m"){
+                html += `<div id="medium">I am medium sized</div>`
+                }
+                if(pet.size.$t == "l"){
+                html += `<div id="large">I am large</div>`
+                }
+                if(pet.size.$t == "xl"){
+                html += `<div id="xl">I am extra large</div>`
+                }
+                html += `<div id="size">${size}</div>`
+                let city = pet.contact.city.$t;
+                html += `<div id="city">${city}</div>`
+                let state = pet.contact.state.$t;
+                html += `<div id="state">${state}</div>`
+
+
+
+                //return zips
+                //let description = pet.description.$t;
+                //html += `<h6>${description}</h6>` 
+
+                //html += '<h5>'+description+ '</h5>'
+                //html +='<img src ="http://photos.petfinder.com/photos/pets/38751785/1/?bust=1499974270&width=50&-t.jpg"/>'
+                //html += '<img src ="http://photos.petfinder.com/photos">'
+                
+                //html += `<h5>${description}</h5>` 
                 //objective is to show all images for each pet. This will require an additional for each loop within the current for each loop.
             } )
-           
+           //for(var k = 0; k<pet.media.photos.photo.length; k++){
+                //if(pet.media.photos.photo[k]['@size'] == "x") {
+                //html +=`<img src = "${pet.media.photos.photo[k].$t}"/>`
+                //}
         
             
 
@@ -62,5 +101,7 @@ function makeApiCall(zipcode){
         {
             alert("Request: "+JSON.stringify(request));
         }
+
     });
+     
 }
