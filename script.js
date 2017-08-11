560d09b30b7bb36933a289ceae7ebafd18732097
Lindsay Potrue <lindsaypotrue@comcast.net>
2017-08-11 12:25:34 -0700

    added advanced

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
  $(document).ready(function(){
 
-    $('form').on('submit', function(e){
-        e.preventDefault(); 
-        console.log("The zip code is: " + $('#zipcode-input').val())
-        makeApiCall($('#zipcode-input').val())
-    })
+  $('form').on('submit', function(e){
+    e.preventDefault(); 
+    console.log("The zip code is: " + $('#zipcode-input').val())
+    makeApiCall($('#zipcode-input').val())
+  })
 
 
 });
@@ -13,159 +13,182 @@
 
 
  var shelters = []; 
-
-
+ 
+ var petDictionary = {};
+//http://api.petfinder.com/pet.find?location=97062&key=a4d4d400939b10647da19b7593286b34&output=full&format=json
 //http://api.petfinder.com/pet.getRandom?format=json&key=a4d4d400939b10647da19b7593286b34&animal=dog&output=basic
-function makeApiCall(zipcode){
+
+function makeApiCall(zipcode, sex, age, size){
 //http://api.petfinder.com/shelter.get?id=SD08&format=json&key=a4d4d400939b10647da19b7593286b34&animal=smallfurry&output=basic
 
 var url = `http://api.petfinder.com/pet.find?location=${zipcode}&key=a4d4d400939b10647da19b7593286b34&output=full&format=json`;
 return $.ajax({
-    type : 'GET',
-    data : {},
-    url : url+'&callback=?' ,
-    dataType: 'json',
-    success : function(data) {              
+  type : 'GET',
+  data : {},
+  url : url+'&callback=?' ,
+  dataType: 'json',
+  success : function(data) {              
 
 
-      shelters = [];
+    shelters = [];
 
 
-      let html = ""
-      let pets = data.petfinder.pets.pet;
-      pets.forEach(function(pet,e) { 
+    let html = ""
+    let pets = data.petfinder.pets.pet;
+    pets.forEach(function(pet,e) { 
+
+
+      petDictionary[pet.id.$t] = pet;
 
 
 
 
+      if(shelters.indexOf(pet.shelterId.$t) < 0) {
+       shelters.push(pet.shelterId.$t)
+     }
 
 
 
-          if(shelters.indexOf(pet.shelterId.$t) < 0) {
-             shelters.push(pet.shelterId.$t)
-         }
-         
 
-
-         //let name = pet.name.$t;
-
-         //html += '<div class="name">'+name+'</div>'
-         if(pet.media.photos){
-             for(var k = 0; k<pet.media.photos.photo.length; k++){
-                 if(pet.media.photos.photo[0]['@size'] == "x") {
-                    html +=`<img class="card-image" src = "${pet.media.photos.photo[k].$t}"/>`
-                   
-               }
-            }
-        }
-
-        /*
-        let breed = pet.breeds.breed.$t;
-        if (pet.mix.$t == "yes") {
-            html += `<p>I am an amazing mixed breed.</p>`
-        }
-        html += `<p>I am a ${breed}</p>`
-        let size = pet.size.$t;
-        if (pet.size.$t == "S") {
-            html += `<p>I am ${size}mall.</p>`
-        }
-        if(pet.size.$t == "M"){
-            html += `<p>I am ${size}edium sized.</p>`
-        }
-        if(pet.size.$t == "L"){
-            html += `<p>I am ${size}arge.</p>`
-        }
-        if(pet.size.$t == "XL"){
-            html += `<p>I am ${size}.</p>`
-        }
-        let age = pet.age.$t;
-        let city = pet.contact.city.$t;
-        let state = pet.contact.state.$t;
-        html += `<p>${age}</p>`
-        html += `<span>And I live in ${city},</span>` 
-        html += `<span>${state}</span>`
-
-        let description = pet.description.$t;
-        html += `<div id ="js-description">${description}<div>` */
-        
-        console.log((e + 1) % 3 === 0)
-        console.log(e + 1)
-
-        if (e % 3 === 0){
+     if (e % 3 === 0){
             //make a new row
-          html += `</div><div class="row">`
+            html += `</div><div class="row">`
 
-        }
+          }
 
-          var DOG_COVER_TEMPLATE = '#dog-template';
-            html +=   
+
+
+          html +=   
           `<div class="col-4">
           <div class="card">
-          <div class="card-content">
-          <img class="card-image" src="${pet.media.photos.photo[0].$t}"/>
+          <div class="card-content">`
           
-          <h3><a onclick="displayProfile(${pet.id.$t})">${pet.name.$t}</a></h3>
+          html += findImage(pet)
+          
+          html += `<h3><a onclick="showPetProfile(${pet.id.$t})">${pet.name.$t}</a></h3>
+          <p>Gender: ${pet.sex.$t}</p>
           <p>I am a ${pet.breeds.breed.$t}.</p>
           <p>${pet.age.$t}</p>
           <span>I live in ${pet.contact.city.$t},<span><span>${pet.contact.state.$t}</span>
-        </div>
-        </div>
-        </div>`
-        console.log(pet)
 
-        //var DOG_PROFILE = '#dog-profile';
-         //html += 
-        // `<p>I am a ${pet.breeds.breed.$t}</p>
-        // <p>${pet.age.$t}</p>
-        // <span>And I live in ${pet.contact.city.$t},</span> 
-        // <span>${pet.contact.state.$t}</span>
-        // <div id ="js-description">${pet.description.$t}<div>` 
+          </div>
+          </div>
+          </div>`
 
-                
+
+
+          console.log(pet)
+
+
         })
 
 
 
-     html = "<div>" + html + "</div>"
 
-      $('#results').html(html)
+
+    html = "<div>" + html + "</div>"
+
+    $('#results').html(html)
       //$('#js-description').html(html)
 
-  },
-  error : function(request,error)
-  {
-    alert("Request: "+JSON.stringify(request));
+    },
+    error : function(request,error)
+    {
+      alert("Request: "+JSON.stringify(request));
+    }
+
+  });
+
 }
 
-});
 
+function findImage(pet){
+   for(var c = 0; c< pet.media.photos.photo.length; c++){
+          if(pet.media.photos.photo[c]['@size'] == "x") {
+            return `<img class="card-image" src = "${pet.media.photos.photo[c].$t}"/>`
+          }
+        }
 }
 
+function showPetProfile(id) {
+    
+        
+    
+
+
+//function showPetProfile(id)
+//{
+
+
+  var dogProfileHtml = 
+  `<div id="name">${petDictionary[id].name.$t}</div>
+  <div id ="js-description">${petDictionary[id].description.$t}<div>
+  <div>I am a ${petDictionary[id].breeds.breed.$t}</div>
+  <div>And I live in ${petDictionary[id].contact.city.$t},</div> 
+  <div>${petDictionary[id].contact.state.$t}</div>
+  <div>Zip Code: ${petDictionary[id].contact.zip.$t}</div>
+  <div>Please call: ${petDictionary[id].contact.phone.$t}</div>
+  <div>Or email: ${petDictionary[id].contact.email.$t}</div>`;
+
+  let breed = petDictionary[id].breeds.breed.$t;
+  if (petDictionary[id].mix.$t == "yes") {
+   dogProfileHtml += `<div class="mix">I am an amazing mixed breed.</div>`
+ }
+ let gender = petDictionary[id].sex.$t;
+ if (petDictionary[id].sex.$t === "M") {
+   dogProfileHtml += `<div class="male">I am a Male.</div>`
+ }
+ if(petDictionary[id].sex.$t === "F"){
+  dogProfileHtml += `<div class="female">I am a Female.</div>`
+}
+
+for(var k = 0; k<petDictionary[id].media.photos.photo.length; k++){
+  if(petDictionary[id].media.photos.photo[k]['@size'] == "x") {
+    dogProfileHtml +=`<img class="profile-image" src = "${petDictionary[id].media.photos.photo[k].$t}"/>`
+  }
+}
+
+
+$("#results").html(dogProfileHtml);
+}
 
 jQuery(document).ready(function (e) {
-    function t(t) {
-        e(t).bind("click", function (t) {
-            t.preventDefault();
-            e(this).parent().fadeOut()
-        })
-    }
-    e(".dropdown-toggle").click(function () {
-        var t = e(this).parents(".button-dropdown").children(".dropdown-menu").is(":hidden");
-        e(".button-dropdown .dropdown-menu").hide();
-        e(".button-dropdown .dropdown-toggle").removeClass("active");
-        if (t) {
-            e(this).parents(".button-dropdown").children(".dropdown-menu").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("active")
-        }
-    });
-    e(document).bind("click", function (t) {
-        var n = e(t.target);
-        if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-menu").hide();
-    });
-    e(document).bind("click", function (t) {
-        var n = e(t.target);
-        if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("active");
+  function t(t) {
+    e(t).bind("click", function (t) {
+      t.preventDefault();
+      e(this).parent().fadeOut()
     })
+  }
+  e(".dropdown-toggle").click(function () {
+    var t = e(this).parents(".button-dropdown").children(".dropdown-menu").is(":hidden");
+    e(".button-dropdown .dropdown-menu").hide();
+    e(".button-dropdown .dropdown-toggle").removeClass("active");
+    if (t) {
+      e(this).parents(".button-dropdown").children(".dropdown-menu").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("active")
+    }
+  });
+  e(document).bind("click", function (t) {
+    var n = e(t.target);
+    if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-menu").hide();
+  });
+  e(document).bind("click", function (t) {
+    var n = e(t.target);
+    if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("active");
+  })
 });
 
+//    makeApiCall($('#zipcode-input').val())
+$('input[type=radio]').change(
+    function(){ 
+var obj = {}
+            $('input[type=radio]:checked' ).each(function () {
+                
+                obj[this.name] = this.value
+            })
+             console.log(obj)
+            makeApiCall($('zipcode-input').val())
+    }
+)
 
+   
 
